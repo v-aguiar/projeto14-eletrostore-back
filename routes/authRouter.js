fdcc5793bbf7a975c13049ca553e1516cdef2408
v-aguiar <v.aguiar.dev@gmail.com>
2022-05-11 16:12:41 -0300

    Validate user on DB, create a session token and returns it together with the username on route POST '/sign-in'

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { registerUser } from "../controllers/authController.js";
+import { registerUser, signIn } from "../controllers/authController.js";
 
 const authRouter = Router();
 
 authRouter.post("/sign-up", registerUser);
+authRouter.post("/sign-in", signIn);
 
 export default authRouter;
---
+++ schemas/sign_in_schema.js
@@ -0,0 +1,8 @@
+﻿import Joi from "joi";
+
+const sign_in_schema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+});
+
+export default sign_in_schema;
